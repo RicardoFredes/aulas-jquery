b0d72f73d5e279c7ef010ed4e9b61c47504a627f
Fredes <ricardofredes@gmail.com>
2019-05-20 22:01:00 -0300

    Update exercicio-11.js

--- publico/js-respostas/exercicio-11.js
+++ publico/js-respostas/exercicio-11.js
@@ -1,35 +1,46 @@
-$('#carregando').hide();
+const api = 'https://api-de-charadas.fredes.now.sh/charada/json';
 $('#instrucoes').hide();
+$('#carregando').hide();
+$('#mensagem-padrao').hide();
 
-const url = 'https://api-de-charadas.fredes.now.sh/charada/json';
-
-$('#botao-carregar-charada').on('click', buscarCharada);
-
-function buscarCharada() {
-	$.ajax({
-		url: url,
-		method: 'GET',
-		dataType: 'json',
-		beforeSend: mostraCarregando,
-		success: carregaCharada,
-	});
+function buscaCharada() {
+  $.ajax({
+    url: api,
+    method: 'GET',
+    beforeSend: mostraCarregando,
+    success: mostrarCharada,
+    error: trataErro,
+  });
 }
 
-function mostraCarregando() {
-	$('#carregando').show();
-	$('#botao-carregar-charada').hide();
+function trataErro (erro) {
+  console.log('Erro: ', erro);
 }
 
-function escondeCarregando() {
-	$('#carregando').hide();
-	$('#botao-carregar-charada').show();
+function mostraCarregando () {
+  // console.log('Carregando...');
+  $('#carregando').show();
+  $('#botao-carregar-charada').hide();
 }
 
-function carregaCharada(dadoRecebido) {
-	console.log(dadoRecebido);
-	const pergunta = dadoRecebido.pergunta;
-	const resposta = dadoRecebido.resposta;
-	$('#pergunta').text(pergunta);
-	$('#resposta').text(resposta);
-	escondeCarregando();
+function mostrarCharada(dadoRecebido) {
+  // console.log(dadoRecebido);
+  const pergunta = dadoRecebido.pergunta;
+  const resposta = dadoRecebido.resposta;
+  $('#pergunta').text(pergunta);
+  $('#resposta').text(resposta);
+
+  $('#resposta').hide();
+  $('#mensagem-padrao').show();
+
+  $('#carregando').hide();
+  $('#botao-carregar-charada').show();
 }
+
+$('#charada').on('click', function () {
+  $('#resposta').toggle();
+  $('#mensagem-padrao').toggle();
+});
+
+// buscaCharada();
+$('#botao-carregar-charada').on('click', buscaCharada);
